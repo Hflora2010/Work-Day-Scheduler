6cbc0321ead8d9bbea520067af1711f8eb14419f
Hailey Flora <Haileycflora@gmail.com>
2023-02-28 18:43:12 -0500

    wrapped time-block function in hourUpdater() add css for header

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,72 +3,81 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 // $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
 
-  $(document).ready(function () {
-    var dayDisplayEl = $("#day-display");
-    var saveBtn = $(".saveBtn");
+$(document).ready(function () {
+  var dayDisplayEl = $("#day-display");
+  var saveBtn = $(".saveBtn");
 
 
-    $(saveBtn).on("click", function () {
+  $(saveBtn).on("click", function () {
     //referencing the specific button for the specific textarea
     var input = $(this).siblings(".description").val();
     // console.log(input)
     var hour = $(this).parent().attr("id");
     // console.log(hour);
-      localStorage.setItem(hour,input);
+    localStorage.setItem(hour, input);
   })
-      //This function displays the current day
-    function displayDay() {
-      var currentDay = dayjs().format("dddd, MMMM D YYYY hh:m a");
-      dayDisplayEl.text(currentDay);
-    }
+  //This function displays the current day
+  function displayDay() {
+    var currentDay = dayjs().format("dddd, MMMM D YYYY");
+    dayDisplayEl.text(currentDay);
+  }
 
-    // function currentTime() {
-    //   var time = dayjs.format("hh:m:s");
-    //   if ()
-    // }
+
+
+
+//askBCS helped me to formulate this function by using .moment()
+  function hourUpdater() {
+    var currentHour = moment().hours();
+  
+    $(".time-block").each(function () {
+      var scheduleHour = parseInt(
+        $(this)
+          .attr("id")
+      );
+  
+      //past
+      if (scheduleHour < currentHour) {
+        $(this).addClass("past");
+  
+        //present
+      } else if (scheduleHour === currentHour) {
+        // $(this).removeClass("past");
+        $(this).addClass(".present");
+  
+        //future
+      } else (scheduleHour > currentHour); {
+        // $(this).removeClass("past");
+        // $(this).removeClass("present");
+        $(this).addClass(".future");
+      }
+    });
+  }
+  hourUpdater();
 
   displayDay();
-  })
+});
 
-  $(".time-block").each(function() {
-    var currentTime = dayjs().format("hh:m:s a");
-    var scheduleHour = parseInt(".time-block").val
-    
-    //past
-    if (scheduleHour < currentTime) {
-    $(".time-block").removeClass ("present");
-    $(".time-block").removeClass("future");
-    // $(this).setAttribute("style", "background: gray");
-    //present
-    } if (scheduleHour === currentTime) {
-    $(".time-block").removeClass("past");
-    $(".time-block").removeClass("future");
-    // $(this).setAttribute("style", "background: red");
-    //future
-    } else (scheduleHour > currentTime) {
-    $(".time-block").removeClass("past");
-    $(".time-block").removeClass("present");
-    }
-  })
-    // $(this).setAttribute("style", "background: green");
 
-  //Getting items from local storage to save on the refresh.
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+//Getting items from local storage to save on the refresh.
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
 
@@ -84,7 +93,3 @@
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-
-
-
-
