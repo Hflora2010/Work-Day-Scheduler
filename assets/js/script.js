b836d7aafd7a4bb6f2cfc5f0114234076aff39cd
Hailey Flora <Haileycflora@gmail.com>
2023-02-28 21:40:42 -0500

    update id in getting items from local storage to save on refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,10 +29,7 @@ $(document).ready(function () {
     dayDisplayEl.text(currentDay);
   }
 
-
-
-
-//askBCS helped me to formulate this function by using .moment()
+  // askBCS helped me to formulate this function by using .moment()
   function hourUpdater() {
     var currentHour = moment().hours();
   
@@ -66,16 +63,17 @@ $(document).ready(function () {
 
 
 
+
 //Getting items from local storage to save on the refresh.
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
   //
